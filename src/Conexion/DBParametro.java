e7206fed182413fff638aef112913c0fb5d90aa6
Diego de la Cruz <diegodlctorres@gmail.com>
2021-04-13 17:01:45 -0500

    Se implementó el JavaDoc de las clases del package de Conexion

--- src/Conexion/DBParametro.java
+++ src/Conexion/DBParametro.java
@@ -1,26 +1,59 @@
 package Conexion;
 
+/**
+ * <h1>DBParametro</h1>
+ * <p>
+ * Es una clase que define objetos que poseen atributos generales para poder ser 
+ * enviados en la ejecución de un stored procedure.
+ *
+ * @author Diego D.
+ * @version 1.1
+ * @since 2021
+ */
 public class DBParametro {
+    
     private String nombre;
     private Object valor;
 
+    /***
+     * Es un método constructor de la clase y recibe dos parámetros de tipo String
+     * y de tipo Object para asignarselo a los atributos de la nueva instancia, dígase, objeto.
+     * @param nombre
+     * @param valor 
+     */
     public DBParametro(String nombre, Object valor) {
         this.nombre = nombre;
         this.valor = valor;
     }
 
+    /***
+     * 
+     * @return {@code nombre}
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /***
+     * 
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /***
+     * 
+     * @return {@code valor}
+     */
     public Object getValor() {
         return valor;
     }
 
+    /***
+     * 
+     * @param valor 
+     */
     public void setValor(Object valor) {
         this.valor = valor;
     }    
